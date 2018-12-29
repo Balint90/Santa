5952a8759dd9842afac833a695515b1673d40a1d
Bálint Csertán <csertanb@gmail.com>
2018-12-29 17:28:09 +0100

    2/b kész

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ while (!feof($myfile)) {
 fclose($myfile);
 
 $sumsquare = 0;
+$sumribbon = 0;
 
 foreach ($arr as $line) {
     $line = preg_replace('/\s+/', '', $line);
@@ -17,9 +18,11 @@ foreach ($arr as $line) {
     $dim = array($l, $w, $h);
     sort($dim);
     $plus = $dim[0]*$dim[1];
+    $sumribbon += (2 * $dim[0]) + (2 * $dim[1]) + ($l*$w*$h);
     $sumsquare += $square + $plus;
 }
 
-echo $sumsquare;
+echo $sumsquare . "<br />";
+echo $sumribbon;
 
 ?>
